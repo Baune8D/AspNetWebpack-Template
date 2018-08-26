ac435884d6fe500beb766d03abe6e4c421e955ba
Morten Larsen <baunegaard@live.dk>
2018-08-26 13:25:05 +0200

    Make Home/Index include a view specific stylesheet

--- AspNetMvcWebpack/Assets/views/Home/Index/index.js
+++ AspNetMvcWebpack/Assets/views/Home/Index/index.js
@@ -1,4 +1,6 @@
 /* This file can be accessed with the alias: Views/Home/Index */
 /* This file will automatically be included in Home/Index view */
 
+import './style.scss';
+
 // Write your JavaScript code.
