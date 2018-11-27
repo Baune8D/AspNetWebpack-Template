46557957e07700a2e4be87403862cf4cb8cc7a38
Morten Larsen <baunegaard@live.dk>
2018-11-27 10:46:07 +0100

    Try to simplify folder setup

--- AspNetMvcWebpack/Assets/views/Shared/_CookieConsentPartial/index.js
+++ AspNetMvcWebpack/Assets/views/Shared/_CookieConsentPartial/index.js
@@ -1,6 +1,6 @@
 /* This file can be accessed with the alias: Views/Shared/_CookieConsentPartial */
 
-import { getValue } from 'Modules/utilities';
+import { getValue } from 'Scripts/utilities';
 
 if (getValue('#showCookieBanner').asBool()) {
   import('Scripts/cookie-consent');
