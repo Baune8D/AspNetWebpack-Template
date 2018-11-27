f3cdca4436f9ff3bc7f4b4bb238c974b10c4fb75
Morten Larsen <baunegaard@live.dk>
2018-11-27 11:15:06 +0100

    Minor cleanup

--- AspNetMvcWebpack/Assets/vendor/vendor.js
+++ AspNetMvcWebpack/Assets/vendor/vendor.js
@@ -1,4 +1,6 @@
 /* This file can be accessed with the alias: Vendor/vendor */
 
-// Dont write your JavaScript code.
+// Don't write your JavaScript code.
 // This folder should only contain 3rd party code.
+
+// All files in this folder will be excluded from Prettier, ESLint and StyleLint.
